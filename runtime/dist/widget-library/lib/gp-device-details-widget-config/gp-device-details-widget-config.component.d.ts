2a050c64ad112447092a37b5255561e68d55e47a
sandhyadeeps <sandhyadeeps@gmail.com>
2022-03-15 17:03:36 +0530

    Deleted node modules

--- runtime/dist/widget-library/lib/gp-device-details-widget-config/gp-device-details-widget-config.component.d.ts
+++
@@ -1,6 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class GpDeviceDetailsWidgetConfigComponent implements OnInit {
-    config: any;
-    constructor();
-    ngOnInit(): void;
-}
--- runtime/dist/widget-library/public-api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/gp-device-details-widget.service';
-export * from './lib/gp-device-details-widget.component';
-export * from './lib/gp-device-details-widget.module';
